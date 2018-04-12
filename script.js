522765954650e3f7e16b976927259afdb1bf52c8
mnap <learn@10g.pl>
2018-04-12 21:17:22 +0200

    Add example scripts

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 'use strict';
 
 var sayHello = function sayHello() {
-  return console.log('Hello World!');
+    return console.log('Hello World!');
 };
 sayHello();
+
+for (var i = 0; i < 5; i++) {
+    console.log(i);
+}
+
+var greeting = 'Hello User!';
+function greetWorld(isGreeting) {
+    if (isGreeting) {
+        var _greeting = 'Hello World!';
+        return _greeting;
+    }
+    return greeting;
+}
+console.log(greetWorld(false)); // 'Hello User!'
+
+var numbers = [1, 2, 3];
+var numbersPlusOne = numbers.map(function (x) {
+    return x + 1;
+});
+console.log(numbersPlusOne);
--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,20 @@
 const sayHello = () => console.log('Hello World!');
 sayHello();
+
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+
+let greeting = 'Hello User!';
+function greetWorld(isGreeting) {
+    if (isGreeting) {
+        let greeting = 'Hello World!';
+        return greeting;
+    }
+    return greeting;
+}
+console.log(greetWorld(false)); // 'Hello User!'
+
+const numbers = [1, 2, 3];
+const numbersPlusOne = numbers.map( x => x + 1 );
+console.log(numbersPlusOne);
