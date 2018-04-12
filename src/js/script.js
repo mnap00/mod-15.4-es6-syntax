8eae33d586b94d43e0f3e4c7b27871277321aaf5
mnap <learn@10g.pl>
2018-04-12 21:25:15 +0200

    Add strings concatenation - task 1

--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,3 @@
-const sayHello = () => console.log('Hello World!');
-sayHello();
-
-for (let i = 0; i < 5; i++) {
-    console.log(i);
-}
-
-let greeting = 'Hello User!';
-function greetWorld(isGreeting) {
-    if (isGreeting) {
-        let greeting = 'Hello World!';
-        return greeting;
-    }
-    return greeting;
-}
-console.log(greetWorld(false)); // 'Hello User!'
-
-const numbers = [1, 2, 3];
-const numbersPlusOne = numbers.map( x => x + 1 );
-console.log(numbersPlusOne);
+const a = 'Hello';
+const b = 'World';
+console.log(`${a} ${b}!`);
