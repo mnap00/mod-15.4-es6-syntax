71f85c8075e493db28764dd9725a591db5773f8b
mnap <learn@10g.pl>
2018-04-12 21:35:17 +0200

    Add multiply function - task 2

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 'use strict';
 
+/* eslint-disable no-console */
+
+// task 1
 var a = 'Hello';
 var b = 'World';
+
 console.log(a + ' ' + b + '!');
+
+// task 2
+var multiply = function multiply() {
+  var a = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 1;
+  var b = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 1;
+  return a * b;
+};
+console.log(multiply(2, 5));
+console.log(multiply(6, 6));
+console.log(multiply(5));
--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,14 @@
+/* eslint-disable no-console */
+
+// task 1
 const a = 'Hello';
 const b = 'World';
+
 console.log(`${a} ${b}!`);
+
+
+// task 2
+const multiply = (a = 1, b = 1) => a * b;
+console.log(multiply(2, 5));
+console.log(multiply(6, 6));
+console.log(multiply(5));
